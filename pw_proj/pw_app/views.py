2a98d20d056e24de5b34c2087779f904966bccef
Rowan Chapman <rchapman83@users.noreply.github.com>
2018-04-04 17:35:44 +1000

    Delete views.py

--- pw_proj/pw_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-# Create your views here.
-def index(request):
-	# return HttpResponse("Hello, world.")
-	template = loader.get_template('pw_app/index.html')
-	return HttpResponse(template.render(request))
