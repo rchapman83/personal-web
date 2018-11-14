dd259e5e4607b83b9f1137adbde008b2c4120c1f
rchapman83 <rchapman83@gmail.com>
2018-11-14 07:52:30 +0000

    update timber stuff

--- run.py
+++ run.py
@@ -10,13 +10,12 @@ x = environ.get('APP_MODE')
 c = environ.get('APP_CONFIG')
 # Get which app to run
 a = environ.get('APP_MODULE')
-# Timber api token and set logging level
-lg = environ.get('TIMBER_TOKEN')
-lv = environ.get('TIMBER_LEVEL')
+# Timber api token
+t = environ.get('TIMBER_TOKEN')
 
 # Never allow more than 50 outstanding log events in buffer and send any outstanding log events at most every 60 seconds
 logger = logging.getLogger(__name__)
-timber_handler = timber.TimberHandler(api_key=lg, level=lv, buffer_capacity=50, flush_interval=60)
+timber_handler = timber.TimberHandler(api_key=t, level=logging.DEBUG, buffer_capacity=50, flush_interval=60)
 logger.addHandler(timber_handler)
 logger.debug('test msg')
 
