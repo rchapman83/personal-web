746891bbe9704e1456a9d1808eae1e45fff8b126
rchapman83 <rchapman83@gmail.com>
2018-11-14 08:13:45 +0000

    remove timber prams

--- run.py
+++ run.py
@@ -15,7 +15,8 @@ t = environ.get('TIMBER_TOKEN')
 
 # Never allow more than 50 outstanding log events in buffer and send any outstanding log events at most every 60 seconds
 logger = logging.getLogger(__name__)
-timber_handler = timber.TimberHandler(api_key=t, level=logging.DEBUG, buffer_capacity=50, flush_interval=60, raise_exceptions=True)
+# timber_handler = timber.TimberHandler(api_key=t, level=logging.DEBUG, buffer_capacity=50, flush_interval=60, raise_exceptions=True)
+timber_handler = timber.TimberHandler(api_key=t, level=logging.DEBUG)
 logger.addHandler(timber_handler)
 logger.debug('test msg')
 
