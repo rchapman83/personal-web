c3aaf469c8b6ad8740d1a20138634c02ff28d1a9
rchapman83 <rchapman83@gmail.com>
2019-02-27 06:43:00 +0000

    remove dest log file

--- app/settings.py
+++ app/settings.py
@@ -16,6 +16,6 @@ SECRET_KEY = secret_code
 
 # LOGGING
 LOGGER_NAME = '%s_log' % proj_name
-LOG_FILENAME = '/mnt/data/logs/flask/pw/app.%s.log' % proj_name
+#LOG_FILENAME = '/mnt/data/logs/flask/pw/app.%s.log' % proj_name
 LOG_LEVEL = logging.INFO
 LOG_FORMAT = '%(asctime)s %(levelname)s\t: %(message)s'
