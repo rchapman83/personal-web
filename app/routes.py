29e0cb9adc9f6ffd16198b6d0541b1b98da91cef
rchapman83 <rchapman83@gmail.com>
2020-01-06 03:52:16 -0800

    added a logger and update status

--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,21 @@ from flask import Flask, render_template, send_from_directory, request
 from os import environ
 # Import code for encoding urls and generating md5 hashes
 import urllib, hashlib
-# Importing logging libs
-import logging, timber
+# Importing python logging libs
+import logging
+# Imports the Google Cloud client library
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+
+# Instantiates a logging client and handler
+client = google.cloud.logging.Client()
+handler = CloudLoggingHandler(client)
+
+# Configure logger and handler
+cloud_logger = logging.getLogger('cloudLogger')
+cloud_logger.setLevel(logging.INFO)
+cloud_logger.addHandler(handler)
+cloud_logger.error('Test MGS - bad news')
 
 # Define custom functions
 # Funtion to generate the URL my gravatar profile picture on the home page
@@ -24,21 +37,12 @@ def gravatar():
 @application.route('/')
 def entry():
     x = gravatar()
-    return render_template('index.html', profilePic=x)
+    return render_template('index.html', profilePic=x), 200
 
 @application.route('/colophon')
 @application.route('/colophon/')
 def colophon():
-    return render_template('colophon.html')
-
-@application.route('/test')
-def test():
-    t = environ.get('TIMBER_TOKEN')
-    logger = logging.getLogger(__name__)
-    timber_handler = timber.TimberHandler(api_key=t)
-    logger.addHandler(timber_handler)
-    logger.warning('test msg')
-    return render_template('test.html')
+    return render_template('colophon.html'), 200
 
 @application.route('/status')
 def status_check():
@@ -46,19 +50,19 @@ def status_check():
     
 @application.route('/robots.txt')
 def robots_static():
-    return send_from_directory(application.static_folder, request.path[1:])
+    return send_from_directory(application.static_folder, request.path[1:]), 200
 
 @application.route('/humans.txt')
 def human_static():
-    return send_from_directory(application.static_folder, request.path[1:])
+    return send_from_directory(application.static_folder, request.path[1:]), 200
 
 @application.route('/keybase.txt')
 def keybase_static():
-    return send_from_directory(application.static_folder, request.path[1:])
+    return send_from_directory(application.static_folder, request.path[1:]), 200
 
 @application.route('/sitemap.xml')
 def sitemap_static():
-    return send_from_directory(application.static_folder, request.path[1:])
+    return send_from_directory(application.static_folder, request.path[1:]), 200
 
 @application.errorhandler(404)
 def not_found(error):
