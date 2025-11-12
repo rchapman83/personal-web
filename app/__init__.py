b9939fe735c3d8e5fdc903cb546ec3b149626bc1
Rowan Chapman <rchapman83@users.noreply.github.com>
2025-11-12 06:23:46 +0000

    talisman added

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,40 @@
 
 # Flask mod imports
 from flask import Flask
+from flask_talisman import Talisman
+
+# Content Security Policy (CSP) Header
+csp = {
+    'default-src': [
+        '\'self\'',
+        'https://www.gravatar.com/', 
+        'https://www.googletagmanager.com',
+        'https://fonts.googleapis.com'
+    ]
+}
+# HTTP Strict Transport Security (HSTS) Header
+hsts = {
+    'max-age': 31536000,
+    'includeSubDomains': True
+}
 
 # Construct application
 # Should the env var not be set you can use application.config.from_pyfile('SETTING FILE')
 application = Flask(__name__.split('.')[0], static_folder='static', template_folder='templates')
 application.config.from_envvar('FLASK_SETTINGS')
+talisman = Talisman(application)
+
+# Enforce HTTPS and other headers
+talisman.force_https = True
+talisman.force_file_save = True
+talisman.x_xss_protection = True
+talisman.session_cookie_secure = True
+talisman.session_cookie_samesite = 'Lax'
+talisman.frame_options_allow_from = 'https://www.google.com/'
+
+# Add the headers to Talisman
+talisman.content_security_policy = csp
+talisman.strict_transport_security = hsts
 
 # Import all the views for this web app
 from . import routes
